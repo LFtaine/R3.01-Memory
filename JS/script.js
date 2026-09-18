28cb3cc68b7dcaf1e1805ae671932dc4449b7003
FONTAINE Louis <fontain242@unicaen.fr>
2026-09-18 10:20:39 +0200

    les images s'affichent dans l'html, fin de la 2.5

--- JS/script.js
+++ JS/script.js
@@ -3,14 +3,33 @@ let imgStart= Math.trunc(Math.random()*100);
 
 let images=[];
 for(let i=0; i<8;i++){
+    imgStart= Math.trunc(Math.random()*100);
     images[i]=`https://picsum.photos/${imgStart}/${dimension}`;
 }
 
 let cards= [...images,...images];
 
+let contenant=document.getElementById("contenant");
+
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]; // On échange les deux places
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // On échange les deux places
     }
 }
+
+function initGame(){
+    shuffle(cards);
+    cards.forEach(creation_images);
+}
+
+
+function creation_images(url){
+    let img=document.createElement("div");
+    img.className="card";
+    img.style.backgroundImage=`url(${url})`;
+    contenant.appendChild(img);
+
+}
+
+initGame();
